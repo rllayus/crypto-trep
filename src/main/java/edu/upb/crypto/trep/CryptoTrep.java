96d684c98a221aabdeda85522019b056c9a3249a
Ricardo Laredo <ricardo@stereum.tech>
2025-02-06 16:05:44 -0400

    first commit

---
+++ src/main/java/edu/upb/crypto/trep/CryptoTrep.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.upb.crypto.trep;
+
+/**
+ *
+ * @author rlaredo
+ */
+public class CryptoTrep {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
